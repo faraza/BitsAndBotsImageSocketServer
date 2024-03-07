2d34e12a64c5fecd1be63c68ae8f03313fb378e3
Faraz Abidi <faraz.abidi@gmail.com>
2024-03-07 10:50:46 -0800

    Added firebase config.

---
+++ src/firestoreSetup.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyD6-aU_uME6OioiQdJZx22F4LItcxUiKW8',
+    authDomain: "bitsandbots.firebaseapp.com",
+    projectId: "bitsandbots",
+    storageBucket: "bitsandbots.appspot.com",
+    messagingSenderId: "889167914862",
+    appId: "1:889167914862:web:8decea2efcb8e69d68c37f",
+    measurementId: "G-F9WE4J599Q"
+};
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
